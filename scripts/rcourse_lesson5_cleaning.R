653278f5a86f1b9b092dbae9ee63bbb717450501
Page Piccinini <page.piccinini@gmail.com>
2016-04-07 08:48:42 +0200

    Update file names.

--- scripts/rcourse_lesson5_cleaning.R
+++ scripts/rcourse_lesson5_cleaning.R
@@ -10,10 +10,10 @@ data_election_results = list.files(path = "data/elections", full.names = T) %>%
   reduce(rbind)
 
 # Read in extra data about specific elections
-data_elections = read.table("data/rcourse_lesson4_data_elections.txt", header=T, sep="\t")
+data_elections = read.table("data/rcourse_lesson5_data_elections.txt", header=T, sep="\t")
 
 # Read in extra data bout specific states
-data_states = read.table("data/rcourse_lesson4_data_states.txt", header=T, sep="\t")
+data_states = read.table("data/rcourse_lesson5_data_states.txt", header=T, sep="\t")
 
 # See how many states in union versus confederacy
 xtabs(~civil_war, data_states)
